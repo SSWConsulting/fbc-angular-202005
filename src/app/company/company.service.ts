06934526e1709b1ef8787ca954e5422471600c64
JeanThirion <jean.thirion@esial.net>
2020-05-14 16:07:31 +1000

    Added Edit Form

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Company } from './company';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -26,6 +26,31 @@ export class CompanyService {
     console.log('SERVICE - calling DeleteCompany', company);
 
     return this.httpClient.delete<Company>(`${this.baseUrl}/company/${company.id}`)
+    .pipe(
+      catchError(error => this.errorHandler(error))
+    );
+  }
+
+  addCompany(company: Company): Observable<Company> {
+    return this.httpClient.post<Company>(`${this.baseUrl}/company`,
+    company,
+    { headers: new HttpHeaders().set('content-type', 'application/json') })
+    .pipe(
+      catchError(error => this.errorHandler(error))
+    );
+  }
+
+  getCompany(companyId: number): Observable<Company> {
+    return this.httpClient.get<Company>(`${this.baseUrl}/company/${companyId}`)
+    .pipe(
+      catchError(error => this.errorHandler(error))
+    );
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this.httpClient.put<Company>(`${this.baseUrl}/company/${company.id}`,
+    company,
+    { headers: new HttpHeaders().set('content-type', 'application/json') })
     .pipe(
       catchError(error => this.errorHandler(error))
     );
